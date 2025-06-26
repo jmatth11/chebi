87076fea52e977446737491454842b7f2965a19f
Joshua Matthews <jmatth93@gmail.com>
2025-06-25 20:39:43 -0500

    starting server

---
+++ src/server.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const manager = @import("manager.zig");
+
+pub const Server = struct {
+    alloc: std.mem.Allocator,
+    manager: manager.Manager,
+
+    pub fn init(alloc: std.mem.Allocator) Server {
+        const result: Server = .{
+            .alloc = alloc,
+            .manager = manager.Manager.init(alloc),
+        };
+        return result;
+    }
+};
