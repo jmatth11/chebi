8af5bf8248f5d51d26443d127dd2a13132f7cb49
Joshua Matthews <jmatth93@gmail.com>
2025-07-04 16:49:59 -0500

    reworked poll to be comptime type

--- src/server.zig
+++ src/server.zig
@@ -1,14 +1,19 @@
 const std = @import("std");
 const manager = @import("manager.zig");
+const poll = @import("poll.zig");
+
+const poll_ctx = poll.Poll(100);
 
 pub const Server = struct {
     alloc: std.mem.Allocator,
     manager: manager.Manager,
+    poll: poll_ctx,
 
-    pub fn init(alloc: std.mem.Allocator) Server {
+    pub fn init(alloc: std.mem.Allocator) !Server {
         const result: Server = .{
             .alloc = alloc,
             .manager = manager.Manager.init(alloc),
+            .poll = try poll_ctx.init(),
         };
         return result;
     }
