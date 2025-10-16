7665babcc921dccfd60fdceb831f2e9b66a3905b
Joshua Matthews <jmatth93@gmail.com>
2025-10-16 15:52:30 -0500

    trying to thread the message processing

--- src/handler.zig
+++ src/handler.zig
@@ -8,6 +8,10 @@ const TryAgainInfo = struct {
     from: std.c.fd_t,
     collection: packet.PacketCollection,
     to: std.c.fd_t,
+
+    pub fn deinit(self: *TryAgainInfo) void {
+        self.collection.deinit();
+    }
 };
 
 const HandlerInfoList = std.array_list.Managed(PacketHandlerInfo);
@@ -19,12 +23,18 @@ pub const Error = error{
     /// This is issued if a would_block occurs.
     try_again,
     errno,
+    process_failed,
 };
 
 pub const PacketHandlerInfo = struct {
     from: std.c.fd_t,
     collection: packet.PacketCollection,
     recipients: RecipientList,
+
+    pub fn deinit(self: *PacketHandlerInfo) void {
+        self.collection.deinit();
+        self.recipients.deinit();
+    }
 };
 
 const MAX_ATTEMPTS: comptime_int = 5;
@@ -33,49 +43,39 @@ pub const PacketHandler = struct {
     alloc: std.mem.Allocator,
     collection: HandlerInfoList,
     try_again_list: TryAgainList,
-    //mutex: std.Thread.Mutex,
-    //pool: std.Thread.Pool,
+    pool: std.Thread.Pool,
+    try_again_mutex: std.Thread.Mutex = .{},
+    pool_error: bool = false,
 
     pub fn init(alloc: std.mem.Allocator) !PacketHandler {
-        //const options: std.Thread.Pool.Options = .{
-        //    .allocator = alloc,
-        //};
-        //var pool: std.Thread.Pool = .{
-        //    .allocator = undefined,
-        //    .threads = undefined,
-        //    .ids = undefined,
-        //};
-        //try std.Thread.Pool.init(&pool, options);
+        const options: std.Thread.Pool.Options = .{
+            .allocator = alloc,
+        };
+        var pool: std.Thread.Pool = .{
+            .allocator = undefined,
+            .threads = undefined,
+            .ids = undefined,
+        };
+        try pool.init(options);
         return .{
             .alloc = alloc,
             .collection = HandlerInfoList.init(alloc),
             .try_again_list = TryAgainList.init(alloc),
-            //.pool = pool,
-            //.mutex = .{},
+            .pool = pool,
         };
     }
 
+    /// Push the structure onto the processing queue.
     pub fn push(self: *PacketHandler, entry: PacketHandlerInfo) !void {
-        // TODO this approach was when it's parallelized
-        // we also insert at the beginning to treat it like a Queue.
-        // But this approach might change
-        //self.mutex.lock();
-        //defer self.mutex.unlock();
-        //try self.collection.insert(0, entry);
         try self.collection.append(entry);
     }
 
-    fn process_single(self: *PacketHandler, from: std.c.fd_t, collection: *const packet.PacketCollection, to: std.c.fd_t) !void {
-        for (collection.packets.items) |payload| {
-            // skip ourselves
-            if (from == to) {
-                return;
-            }
-            try self.send(to, payload);
-        }
-    }
-
+    /// Process the message, related to the given ID, that failed in it's initial send.
+    /// This function increments the retry counter on the message. Once
+    /// the message reaches MAX_ATTEMPTS on the retry counter, it is dropped.
     pub fn process_eagain(self: *PacketHandler, id: std.c.fd_t) !void {
+        self.try_again_mutex.lock();
+        defer self.try_again_mutex.unlock();
         for (self.try_again_list.items, 0..) |*item, idx| {
             if (item.to == id) {
                 item.*.attempts += 1;
@@ -92,7 +92,7 @@ pub const PacketHandler = struct {
                     if (item.attempts == MAX_ATTEMPTS) {
                         std.log.err("dropping message from {}\n", .{item.from});
                     }
-                    item.*.collection.deinit();
+                    item.*.deinit();
                     _ = self.try_again_list.swapRemove(idx);
                 }
                 return;
@@ -100,30 +100,19 @@ pub const PacketHandler = struct {
         }
     }
 
+    /// Process all pending messages that are ready to send.
     pub fn process(self: *PacketHandler) !void {
+        var wg: std.Thread.WaitGroup = .{};
         for (self.collection.items) |*info| {
-            for (info.recipients.items) |id| {
-                self.process_single(info.from, &info.collection, id) catch |err| {
-                    if (err == Error.try_again) {
-                        const entry: TryAgainInfo = .{
-                            .to = id,
-                            .from = info.from,
-                            .collection = try info.collection.dupe(),
-                        };
-                        try self.try_again_list.append(entry);
-                    } else {
-                        return err;
-                    }
-                };
-            }
-            // free collection once done
-            info.*.collection.deinit();
-            info.recipients.deinit();
+            self.pool.spawnWg(&wg, PacketHandler.process_threaded, .{ self, info });
+        }
+        wg.wait();
+        if (self.pool_error) {
+            return Error.process_failed;
         }
         // reset collection.
         try self.collection.resize(0);
     }
-
 
     /// Send the packet to the given socket.
     pub fn send(self: *PacketHandler, socket: std.c.fd_t, payload: packet.Packet) !void {
@@ -138,6 +127,7 @@ pub const PacketHandler = struct {
     }
 
     pub fn deinit(self: *PacketHandler) void {
+        self.pool.deinit();
         if (self.collection.items.len > 0) {
             for (self.collection.items) |*item| {
                 item.*.collection.deinit();
@@ -149,5 +139,49 @@ pub const PacketHandler = struct {
             }
         }
         self.collection.deinit();
+    }
+
+    fn process_threaded(self: *PacketHandler, info: *PacketHandlerInfo) void {
+        for (info.recipients.items) |id| {
+            self.process_single(info.from, &info.collection, id) catch |err| {
+                if (err == Error.try_again) {
+                    if (info.collection.dupe()) |col_dupe| {
+                        const entry: TryAgainInfo = .{
+                            .to = id,
+                            .from = info.from,
+                            .collection = col_dupe,
+                        };
+                        self.append_try_again(entry) catch |append_err| {
+                            std.log.err("Process handler appending to try again failed: {any}\n", .{append_err});
+                            self.pool_error = true;
+                        };
+                    } else |dupe_err| {
+                        std.log.err("collection dupe failed: {any}\n", .{dupe_err});
+                        self.pool_error = true;
+                    }
+                } else {
+                    std.log.err("process_threaded failed: {any}\n", .{err});
+                    self.pool_error = true;
+                }
+            };
+        }
+        // free collection once done
+        info.*.deinit();
+    }
+
+    fn process_single(self: *PacketHandler, from: std.c.fd_t, collection: *const packet.PacketCollection, to: std.c.fd_t) !void {
+        for (collection.packets.items) |payload| {
+            // skip ourselves
+            if (from == to) {
+                return;
+            }
+            try self.send(to, payload);
+        }
+    }
+
+    fn append_try_again(self: *PacketHandler, entry: TryAgainInfo) !void {
+        self.try_again_mutex.lock();
+        defer self.try_again_mutex.unlock();
+        try self.try_again_list.append(entry);
     }
 };
